e0b5cd8ebda644096fcd8c7dabea509911a07de3
Devin Cao <devincao@csu.fullerton.edu>
2019-02-19 15:05:36 -0800

    Added new methods I learned and fixed some bugs
    Testing find method
    for looping
    fixed delimiter

--- student.py
+++ student.py
@@ -4,18 +4,6 @@ students = []
 
 import csv
 
-with open('example_students.csv',newline='') as csvfile:
-    reader = csv.reader(csvfile,delimiter = ' ')
-    line_count = 0
-    for row in reader:
-        if line_count == 0:
-            print(f"Column names are {", ".join(row)}")
-            line_count += 1
-        else:
-            students.append(Student({row[0]},{row[1]},{row[2]},{row[3]},{row[4]},{row[5]},{row[6]})
-            line_count += 1
-    print(f'Processed {line_count} lines.')
-
 class Student:
     def __init__(self, fName,mName,lName, age, guid,grade,gpa):
         self.fName = fName
@@ -27,10 +15,35 @@ class Student:
         self.gpa = gpa
 
     def printName(self):
-        return (self.fName+" " + self.mName+ " " + self.lName)
+        return (str(self.fName)+" " + str(self.mName)+ " " + str(self.lName))
 
-print("There are " + line_count + " students")
-print(students[0])
+with open('example_students.csv',newline='') as csvfile:
+    reader = csv.reader(csvfile,delimiter = ',')
+    line_count = 0
+    for row in reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            # print({row[0]},{row[1]},{row[2]},{row[3]},{row[4]},{row[5]},{row[6]})
+            students.append(Student({row[0]},{row[1]},{row[2]},{row[3]},{row[4]},{row[5]},{row[6]}))
+            line_count += 1
+    print(f'Processed {line_count} lines.')
+
+print("There are " + str(line_count) + " students")
+# print(vars(students[0]))
+
+for student in students:
+    print(student.printName())
+    print(student.guid)
+
+searchID = input("Enter ID of student to identify: ")
+for student in students:
+    if str(student.fName).find(searchID) != -1:
+        print(student.fName)
+    # else:
+        # print(student.mName)
+print("Searched ID: " + searchID)
 # p1 = Student("John","Dinkle","Ain", 36,1,2.5)
 
 # print(p1.name)
